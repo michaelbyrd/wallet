c8609626a0b3b829448ddba1e0cf103f75e12030
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-29 10:36:56 -0500

    add validation to transaction; use number_to_currency in index.html.erb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,10 @@
 class Transaction < ActiveRecord::Base
+  validates :amount, presence: true
+  validates :account_type, presence: true
   def self.balance(arr)
     arr.reduce(0.0) { |s, t| s += t.amount }
   end
+
   def self.checkings
     Transaction.where(account_type: "Checkings")
   end
